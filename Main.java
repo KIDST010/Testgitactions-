1b96cf2c49569766141aeb264f5c90550d7d1bbf
KIDST010 <73382531+KIDST010@users.noreply.github.com>
2021-06-29 16:36:34 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(print());
+    }
+
+    public static void print(){
+        System.out.println("hello kidst!");
+    }
+}
