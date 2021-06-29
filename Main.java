3ddf5b981706254a7a28ff65dccd5a3611ef732b
KIDST010 <73382531+KIDST010@users.noreply.github.com>
2021-06-29 16:47:05 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(print())
-    }
-
-    public static void print(){
         System.out.println("hello kidst!");
     }
+
+    
 }
